91bfa8faf42a323f931f972837f04452b37c02bb
juanjo698 <44554915+juanjo698@users.noreply.github.com>
2018-12-13 01:27:10 +0100

    Varias keys parece bien

---
+++ State.h
@@ -0,0 +1,86 @@
+#ifndef State_h
+#define State_h
+#include <vector>
+#include <tuple>
+#include <algorithm>
+
+using namespace std;
+
+struct State{
+public:
+	//Reference
+	int id;
+	//Father reference
+	int father_id;
+	//Al x-axis position
+	int al_x_pos;
+	//Al y-axis position
+	int al_y_pos;
+	//Father x-axis position
+	int father_x;
+	//Father y-axis position
+	int father_y;
+	//Key state in this moment
+	vector<tuple<int, int>> state_keys;
+	//Rock state in this moment
+	vector<tuple<int, int>> state_rocks;
+	//f(n) = g(n) + h(n)
+	int f;
+	//g(n) cost to move
+	int g;
+	//h(n) heuristic cost
+	int h;
+	//Allow father generation
+	bool allow_father;
+	//Custom operator for sorting
+	bool operator < (const State &other) const {
+    	return f < other.f;
+    }
+    //Custom operator for find
+    bool operator == (const State& other) const {
+    	bool foundKey = true;
+    	bool foundRock = true;
+    	int counter = 0;
+    	tuple<int, int> rock;
+    	tuple<int, int> key;
+
+    	if((al_x_pos == other.al_x_pos) && (al_y_pos == other.al_y_pos) && (state_keys.size() == other.state_keys.size())){
+    		while((counter < state_keys.size()) && (foundKey)){
+    			key = other.state_keys.at(counter);
+    			if(find(state_keys.begin(), state_keys.end(), key) == state_keys.end()){
+    				foundKey = false;
+    			}
+    			counter++;
+    		}
+
+    		if(foundKey){
+	    		counter = 0;
+
+	    		while((counter < state_rocks.size()) && (foundRock)){
+	    			rock = other.state_keys.at(counter);
+	    			if(find(state_rocks.begin(), state_rocks.end(), rock) == state_rocks.end()){
+	    				foundRock = false;
+	    			}
+	    			counter++;
+	    		}
+    		}
+    	}
+    	return (foundRock && foundKey);
+    }
+	//Default constructor
+	State();
+};
+
+State::State(){
+	id = 0;
+	father_id = 0;
+	father_x = -1;
+	father_y = -1;
+	al_x_pos = 0;
+	al_y_pos = 0;
+	f = 0;
+	g = 0;
+	h = 0;
+	allow_father = false;
+}
+#endif
